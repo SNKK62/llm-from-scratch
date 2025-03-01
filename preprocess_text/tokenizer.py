e22f45d24162af2ba0cad9f52eddbe76f4a957bf
SNKK62 <koki.seno62@gmail.com>
2025-03-01 23:05:34 +0900

    implement BPE tokenizer

--- preprocess_text/tokenizer.py
+++ preprocess_text/tokenizer.py
@@ -1,65 +1,17 @@
-import re
+from importlib.metadata import version
+import tiktoken
 
+print("tiktoken version: ", version("tiktoken"))
 
-class SimpleTokenizerV1:
-    def __init__(self, vocab):
-        self.str_to_int = vocab
-        self.int_to_str = {i: s for s, i in vocab.items()}
+# BPE (Byte Pair Encoding) tokenizer
+tokenizer = tiktoken.get_encoding("gpt2")
 
-    def encode(self, text):
-        preprocessed = re.split(r'([,.?_!"()\']|--|\s)', text)
-        preprocessed = [item for item in preprocessed if item.strip()]
-        ids = [self.str_to_int[token] for token in preprocessed]
-        return ids
+text = (
+    "Hello, do you like tea? <|endoftext|> In the sunlit terracesof someunknownPlace."
+)
 
-    def decode(self, ids):
-        text = " ".join([self.int_to_str[i] for i in ids])
-        text = re.sub(r'\s+([,.?!"()\'])', r"\1", text)
-        return text
+integers = tokenizer.encode(text, allowed_special={"<|endoftext|>"})
+print(integers)
 
-
-class SimpleTokenizerV2:
-    def __init__(self, vocab):
-        self.str_to_int = vocab
-        self.int_to_str = {i: s for s, i in vocab.items()}
-
-    def encode(self, text):
-        preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', text)
-        preprocessed = [item.strip() for item in preprocessed if item.strip()]
-        # Replace unknown words with <|unk|>
-        preprocessed = [
-            item if item in self.str_to_int else "<|unk|>" for item in preprocessed
-        ]
-        ids = [self.str_to_int[s] for s in preprocessed]
-        return ids
-
-    def decode(self, ids):
-        text = " ".join([self.int_to_str[i] for i in ids])
-        text = re.sub(r'\s+([,.:;?!"()\'])', r"\1", text)
-        return text
-
-
-with open("the-verdict.txt", "r") as file:
-    raw_text = file.read()
-
-preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', raw_text)
-preprocessed = [item for item in preprocessed if item.strip()]
-
-all_words = sorted(set(preprocessed))
-
-vocab = {token: integer for integer, token in enumerate(all_words)}
-for special_token in ["<|unk|>", "<|endoftext|>"]:
-    vocab[special_token] = len(vocab)
-
-# tokenizer = SimpleTokenizerV1(vocab)
-tokenizer = SimpleTokenizerV2(vocab)
-
-text1 = "Hello, do you like tea?"
-text2 = "In the sunlit terraces of the palace."
-text = " <|endoftext|> ".join((text1, text2))
-print(text)
-
-ids = tokenizer.encode(text)
-print(ids)
-
-print(tokenizer.decode(ids))
+strings = tokenizer.decode(integers)
+print(strings)
