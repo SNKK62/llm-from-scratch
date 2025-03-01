6c0686d43173fb03e3b1c881abf3b8ed40f95213
SNKK62 <koki.seno62@gmail.com>
2025-03-01 23:28:26 +0900

    show simple embedding layer

--- preprocess_text/dataset.py
+++ preprocess_text/dataset.py
@@ -51,11 +51,10 @@ if __name__ == "__main__":
         raw_text = f.read()
 
     dataloader = create_dataloader_v1(
-        raw_text, batch_size=1, max_length=4, stride=1, shuffle=False
+        raw_text, batch_size=8, max_length=4, stride=4, shuffle=False
     )
     data_iter = iter(dataloader)
-    first_batch = next(data_iter)
-    print(first_batch)
+    inputs, target = next(data_iter)
 
-    second_batch = next(data_iter)
-    print(second_batch)
+    print("Input:\n", inputs)
+    print("Target:\n", target)
