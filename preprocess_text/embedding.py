bac7501de5c3ae28f549e5eddd1307921995023d
SNKK62 <koki.seno62@gmail.com>
2025-03-01 23:38:10 +0900

    implement token and position embeddings

--- preprocess_text/embedding.py
+++ preprocess_text/embedding.py
@@ -1,12 +1,35 @@
 import torch
+from dataset import create_dataloader_v1
 
 input_ids = torch.tensor([2, 3, 5, 1])
 
-vocab_size = 6
-output_dim = 3
+vocab_size = 50257
+output_dim = 256
 
 torch.manual_seed(123)
-embedding_layer = torch.nn.Embedding(vocab_size, output_dim)
-print(embedding_layer.weight)
+token_embedding_layer = torch.nn.Embedding(vocab_size, output_dim)
 
-print(embedding_layer(input_ids))
+max_length = 4
+
+with open("the-verdict.txt", "r") as f:
+    raw_text = f.read()
+
+dataloader = create_dataloader_v1(
+    raw_text, batch_size=8, max_length=max_length, stride=max_length, shuffle=False
+)
+
+data_iter = iter(dataloader)
+inputs, targets = next(data_iter)
+print("Token IDs:\n", inputs)
+print("\nInputs shape:\n", inputs.shape)
+
+token_embeddings = token_embedding_layer(inputs)
+print("\nToken Embeddings shape:\n", token_embeddings.shape)
+
+context_length = max_length
+pos_embedding_layer = torch.nn.Embedding(context_length, output_dim)
+pos_embeddings = pos_embedding_layer(torch.arange(context_length))
+print("\nPositional Embeddings shape:\n", pos_embeddings.shape)
+
+input_embeddings = token_embeddings + pos_embeddings
+print("\nInput Embeddings shape:\n", input_embeddings.shape)
